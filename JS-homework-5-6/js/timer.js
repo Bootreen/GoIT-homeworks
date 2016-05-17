216f99a8dbe57bd55e8e1abf4cb3e40c6a5511ea
Bootreen <bootreen@gmail.com>
2016-05-17 19:28:46 +0300

    JS-homework-5-6 финальная версия

--- JS-homework-5-6/js/timer.js
+++ JS-homework-5-6/js/timer.js
@@ -17,12 +17,16 @@ function handleStartStop() {
     timerStarted = true;
     buttonSplit.removeAttribute('disabled');
     buttonStartStop.textContent = 'Stop';
+    buttonStartStop.classList.remove('button-green');
+    buttonStartStop.classList.add('button-red');
     startTime = Date.now();
     tick();
   } else {
     timerStarted = false;
     buttonSplit.setAttribute('disabled', 'disabled');
     buttonStartStop.textContent = 'Start';
+    buttonStartStop.classList.remove('button-red');
+    buttonStartStop.classList.add('button-green');
     cancelAnimationFrame(tickerHandler);
     postResult ('stop');
   }
@@ -33,16 +37,24 @@ function handleSplit() {
 }
 
 function handleReset() {
+  // Для правильной отработки вызова handleStartStop из сброса
+  // таймер должен считаться запущенным, даже если это не так
   timerStarted = true;
   handleStartStop();
   timerClock.textContent = '00:00:00.000';
   resultCounter = 1;
+  // Методично удаляем все результаты
   while (document.querySelector('h2')) {
     resultsContainer.removeChild(document.querySelector('h2'));
   }
 }
 
 function postResult(resultType) {
+  // Если результатов набралось больше 15, начинаем стирать первые
+  // по стековому принципу, чтобы не рвало верстку страницы
+  if (resultCounter > 15) {
+    resultsContainer.removeChild(document.querySelector('h2'));
+  }
   result = document.createElement('h2');
   result.textContent = resultCounter + ' ' + resultType + ': ' + timerClock.textContent;
   resultsContainer.appendChild(result);
@@ -56,7 +68,7 @@ function tick() {
 
 function getTimeStamp (msecs) {
   function div(a, b) {return [Math.floor(a / b), a % b];}
-  // Проверяем второй аргумент на длину по первому.
+  // Проверяем второй аргумент на длину по первому аргументу.
   // Если длина меньше заданной, рекурсивно вызываем
   // pad и дописываем '0' прямо в вызове
   function pad(c, v) {return v.length < c ? pad(c, '0' + v) : v;}
