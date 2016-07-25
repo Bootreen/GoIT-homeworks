39b8a4f77aa4476e0af91d0f842b7f7db2a90215
Alexei Bootreen <bootreen@gmail.com>
2016-07-25 12:07:18 +0300

    Fixed path v2

--- BBQ4U/src/common/components/Routes.js
+++ BBQ4U/src/common/components/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+// import { Route, IndexRoute } from 'react-router';
+import { Router, Route, Link, IndexRoute } from 'react-router';
 
 import App from './App';
 import HomePage from '../../pages/bbq4u/bbq4u';
 
 
 export default (
-  <Route path="/" component={App}>
+  <Route path="http://bootreen.github.io/bbq4u/build/" component={App}>
     <IndexRoute component={HomePage} />
   </Route>
 );
