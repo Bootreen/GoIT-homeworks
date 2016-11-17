7dccd74613e27bf496104f5028ab512b41442a04
Alexei Bootreen <bootreen@gmail.com>
2016-11-17 11:54:58 +0200

    Доделаны спрайты, убраны глобальные переменные

--- Schmario/js/schmario.js
+++ Schmario/js/schmario.js
@@ -1,25 +1,43 @@
-// Размеры игрового поля
-var CANVAS_WIDTH = 1280,
-    CANVAS_HEIGHT = 960;
+"use strict";
 
-var canvas = document.createElement('canvas'),
-    gameField = canvas.getContext('2d');
+// Небольшая реструктуризация, оформляем по-человечески,
+// избавляемся от глобальных переменных.
+// Остальные комменты потом.
 
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
+var gameApp = function() { // Переменные, константы, прочие данные
+  this.canvas = document.getElementById('canvas');
+  this.gameField = this.canvas.getContext('2d');
 
-document.body.appendChild(canvas);
-
-
-var lastTime;
-// Тикер
-function gameLoop() {
-    var now = Date.now();
-    var dt = (now - lastTime) / 1000.0;
-
-    update(dt);
-    render();
-
-    lastTime = now;
-    requestAnimFrame(gameLoop);
+  this.paths = ["./img/background.jpg", "./img/mario.png", "./img/bricks.png", "./img/coin.png",
+                "./img/mushroom.png", "./img/turtle.png", "./img/evil-flower.png", "./img/fire.png",
+                "./img/flag.png", "./img/bonuses.png"];
+  this.C_WIDTH = 1280;
+  this.C_HEIGHT = 720;
 };
+
+gameApp.prototype = {
+
+  loadImg: function(path) { // Загрузчик спрайтшитов
+    return new Promise(function(resolve, reject) {
+      var img = new Image();
+      img.src = path;
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+    });
+  },
+
+  init: function() {
+    this.canvas.width = this.C_WIDTH;
+    this.canvas.height = this.C_HEIGHT;
+
+    Promise.all(this.paths.map(this.loadImg))
+    // .then(gameLoop)
+    // .catch(errorHandler);
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+  },
+
+}
+
+var schmario = new gameApp();
+schmario.init();
