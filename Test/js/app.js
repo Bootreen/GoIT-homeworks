b5754d81a075f06c21fd947af4cb019458ffb764
Alexei Bootreen <bootreen@gmail.com>
2017-01-23 14:23:29 +0200

    Общая структура

--- Test/js/app.js
+++ Test/js/app.js
@@ -1,9 +1,34 @@
-(function () {
-  var table = '';
-  function format(number) {return number.length < 3 ? format(' ' + number) : number;}
-  for (var i = 1; i <= 10; i++) {
-    for (var j = 1; j <= 10; j++) {table = table + format(i*j + '') + ' ';}
-    table = table + '\n'
+"use strict";
+
+// Потом стырим отсюда форматирование для вывода таблицы
+// (function () {
+//   var table = '';
+//   function format(number) {return number.length < 3 ? format(' ' + number) : number;}
+//   for (var i = 1; i <= 10; i++) {
+//     for (var j = 1; j <= 10; j++) {table = table + format(i*j + '') + ' ';}
+//     table = table + '\n'
+//   }
+//   console.log(table);
+// })();
+
+(function init() {
+
+  var sheet = new Map();
+
+  document.getElementById('submit').onclick = function () {
+    parse(document.getElementById('stdin').value);
   }
-  console.log(table);
+
+  function parse(input) {
+    unparse(input + ' типа обработал )))');
+  }
+
+  function process(table) {
+
+  }
+
+  function unparse(output) {
+    document.getElementById('stdin').value = output;
+  }
+
 })();
