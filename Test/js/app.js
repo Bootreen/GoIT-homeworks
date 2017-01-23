6639c489fa2add492d079636842a1cf13e21c465
Alexei Bootreen <bootreen@gmail.com>
2017-01-23 16:30:49 +0200

    Сделал ввод табуляций в textarea

--- Test/js/app.js
+++ Test/js/app.js
@@ -1,26 +1,83 @@
 "use strict";
 
+// ======== borrowed and adapted from external source
+function getCaretPos(input) {
+  if (input.selectionStart) return input.selectionStart;
+    else if (document.selection) {
+      input.focus();
+      var rangeStart = document.selection.createRange();
+      if (rangeStart == null) return 0;
+      var rangeEnd = input.createTextRange(),
+      rangeCopy = rangeEnd.duplicate();
+      rangeEnd.moveToBookmark(rangeStart.getBookmark());
+      rangeCopy.setEndPoint("EndToStart", rangeEnd);
+      return rangeCopy.input.length;
+    }
+  return 0;
+}
+
+function setCaretToPos(input, selection) {
+  if (input.setCaretToPos) {
+    input.focus();
+    input.setCaretToPos(selection, selection);
+  }
+  else if (input.createTextRange) {
+    var range = input.createTextRange();
+    range.collapse(true);
+    range.moveEnd("character", selection);
+    range.moveStart("character", selection);
+    range.select();
+  }
+}
+
+function insertTextAtCaret(input, text) {
+  var val = input.value, endIndex, range, doc = input.ownerDocument;
+  if (typeof input.selectionStart == "number" && typeof input.selectionEnd == "number") {
+    endIndex = input.selectionEnd;
+    input.value = val.slice(0, endIndex) + text + val.slice(endIndex);
+    input.selectionStart = input.selectionEnd = endIndex + text.length;
+  } else if (doc.selection != "undefined" && doc.selection.createRange) {
+    input.focus();
+    range = doc.selection.createRange();
+    range.collapse(false);
+    range.text = text;
+    range.select();
+  }
+}
+
+//========================================================================================
+
 // Потом стырим отсюда форматирование для вывода таблицы
-// (function () {
-//   var table = '';
 //   function format(number) {return number.length < 3 ? format(' ' + number) : number;}
-//   for (var i = 1; i <= 10; i++) {
-//     for (var j = 1; j <= 10; j++) {table = table + format(i*j + '') + ' ';}
-//     table = table + '\n'
-//   }
-//   console.log(table);
-// })();
+
 
 (function init() {
 
   var sheet = new Map();
 
-  document.getElementById('submit').onclick = function () {
-    parse(document.getElementById('stdin').value);
+  var inoutArea = document.getElementById("stdin"),
+      submitButton = document.getElementById("submit");
+
+  submitButton.onclick = () => parse(inoutArea.value);
+
+  // Custom TAB handler for spreadsheet to prevent focus loose by textarea
+  inoutArea.onkeydown = () => {
+    var onTabDown = true;
+    if (event.keyCode == 9) {
+      var storedPos = getCaretPos(inoutArea);
+      document.onkeyup = () => {
+        if (event.keyCode == 9 && onTabDown) {
+          inoutArea.focus();
+          setCaretToPos(storedPos);
+          insertTextAtCaret(inoutArea, '\t');
+        }
+      }
+    }
   }
 
   function parse(input) {
-    unparse(input + ' типа обработал )))');
+
+    // unparse(input + ' типа обработал )))');
   }
 
   function process(table) {
@@ -28,7 +85,7 @@
   }
 
   function unparse(output) {
-    document.getElementById('stdin').value = output;
+    inoutArea.value = output;
   }
 
 })();
