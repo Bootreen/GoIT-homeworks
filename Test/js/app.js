ac62acfbe3bf2010615c491fecd9a8c3d3ee8bbe
Alexei Bootreen <bootreen@gmail.com>
2017-02-10 19:07:28 +0200

    SKM v0.1

--- Test/js/app.js
+++ Test/js/app.js
@@ -65,8 +65,8 @@
     const result = {};
     for (let r = 0; r < rowCount; r++) {
       const row = sheet[r];
-      // Empty raws and cells are valid
-      if (!row && row != '') return '#Error: insufficient rows';
+      // Empty cells and raws with empty cells are valid
+      if (!row && row != []) return '#Error: insufficient rows';
       // Reverse quotes for calculated expression (ES6 template string)
       if (row.length > colCount) return `#Error: too many cells at the sheet line #${r+1}`;
       for (let c = 0; c < colCount; c++) {
@@ -76,11 +76,106 @@
         result[`${columnIdxToLetters(c)}${r+1}`] = cell.trim();
       }
     }
+    // console.log(result);
     return result;
   }
 
   function calculate(input) {
-    return input;
+
+    // Spread realization krutch for maps
+    function merge() {
+      function mergePair(target, source) {
+        if (source)
+          Object.keys(source).forEach(function (key) {if (source[key]!==undefined) target[key] = source[key];});
+      }
+      let target = arguments[0];
+      if (target) {
+        for (let i = 1, argsLen = arguments.length; i < argsLen; i++) {
+            mergePair(target, arguments[i])
+        }
+      }
+      return target;
+    }
+
+    const parseFormula = formula => {
+      const parsedFormula = formula.match(/\(|\)| |[A-Z]+\d+|\+|-|\*|\/|[-+]*\d+/g);
+      return (parsedFormula && parsedFormula.join("").length == formula.length)
+        ? formula.match(/[A-Z]+\d+/g) || []
+        : null;
+    };
+
+    const evalFormula = (formula, cellNames, cells) =>
+      eval(`((${cellNames.join(",")})=>${formula})(${cellNames.map(cellName => cells[cellName]).join(",")})`) + "";
+
+    const toPoorManSet = list => {
+      const result = {};
+      list.forEach(v => result[v] = true);
+      return result;
+    };
+
+    const dedupe = list => Object.keys((toPoorManSet(list)));
+
+    const isSFormula = v => v && v[0] === "=";
+
+    const isSString = v => v && v[0] === "'";
+
+    const isInvalidNumber = v => isNaN(parseFloat(v));
+
+    const evalSpreadsheet = spreadsheet => {
+      const cellNames = Object.keys(spreadsheet);
+      const evaluatedCells = {};
+      const dependentCells = {};
+      const errorCells = {};
+      cellNames.forEach(cellName => {
+        const cellValue = spreadsheet[cellName];
+        if (isSFormula(cellValue)) {
+          const formula = cellValue.slice(1);
+          const formulaDeps = parseFormula(formula);
+          if (formulaDeps) {
+            if (formulaDeps.length)
+              dependentCells[cellName] = {formula, deps: dedupe(formulaDeps)};
+            else
+              evaluatedCells[cellName] = evalFormula(formula, [], {});
+          } else
+            errorCells[cellName] = "#Invalid formula";
+        } else if (isSString(cellValue))
+          evaluatedCells[cellName] = cellValue.slice(1);
+        else if (isInvalidNumber(cellValue))
+          errorCells[cellName] = "#Invalid number";
+        else
+          evaluatedCells[cellName] = cellValue;
+      });
+      let cellNamesToEval = Object.keys(dependentCells);
+      while (cellNamesToEval.length) {
+        const justEvaluatedCellNames = [];
+        cellNamesToEval.forEach(cellName => {
+          const cell = dependentCells[cellName];
+          const deps = cell.deps;
+          if (!dependentCells[cellName].deps.every(depCellName => evaluatedCells[depCellName] || dependentCells[depCellName] || errorCells[depCellName])) {
+            errorCells[cellName] = "#Reference to empty cell";
+            delete dependentCells[cellName];
+          } else if (deps.some(depCellName => errorCells[depCellName])) {
+            errorCells[cellName] = "#Reference to erroneous cell";
+            delete dependentCells[cellName];
+          } else if (deps.every(depCellName => evaluatedCells[depCellName])) {
+            if (deps.some(depCellName => isInvalidNumber(evaluatedCells[depCellName]))) {
+              errorCells[cellName] = "#Reference to invalid value";
+            } else {
+              evaluatedCells[cellName] = evalFormula(cell.formula, cell.deps, evaluatedCells);
+              justEvaluatedCellNames.push(cellName);
+            }
+            delete dependentCells[cellName];
+          }
+        });
+        if (!justEvaluatedCellNames.length)
+          cellNamesToEval.forEach(cellName => errorCells[cellName] = "#Circular dependency");
+        cellNamesToEval = Object.keys(dependentCells);
+      }
+      console.log(merge({},evaluatedCells,errorCells));
+      return merge({},evaluatedCells,errorCells);
+    }
+
+    return evalSpreadsheet(input);
   }
 
   function unparse(input) {
